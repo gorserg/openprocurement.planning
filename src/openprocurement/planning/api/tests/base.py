830f3cf9d40864c5aedba6b8fabd9116166ddc79
gorserg <gorserg@hotmail.com>
2015-12-03 09:32:11 +0000

    fix tests

--- src/openprocurement/planning/api/tests/base.py
+++ src/openprocurement/planning/api/tests/base.py
@@ -9,7 +9,119 @@ from openprocurement.api import VERSION
 from openprocurement.api.tests.base import PrefixedRequestClass
 
 now = datetime.now()
-test_plan_data = {
+test_plan_data =  {
+        "tender": {
+            "procurementMethod": u"open",
+            "tenderPeriod": {
+                "startDate": (now + timedelta(days=7)).isoformat()
+            }
+        },
+        "items": [
+            {
+                "deliveryDate": {
+                    "endDate": (now + timedelta(days=15)).isoformat()
+                },
+                "additionalClassifications": [
+                    {
+                        "scheme": u"ДКПП",
+                        "id": u"01.11.92",
+                        "description": u"Насіння гірчиці"
+                    }
+                ],
+                "unit": {
+                    "code": u"KGM",
+                    "name": u"кг"
+                },
+                "classification": {
+                    "scheme": u"CPV",
+                    "description": u"Mustard seeds",
+                    "id": u"03111600-8"
+                },
+                "quantity": 1000,
+                "description": u"Насіння гірчиці"
+            },
+            {
+                "deliveryDate": {
+                    "endDate": (now + timedelta(days=16)).isoformat()
+                },
+                "additionalClassifications": [
+                    {
+                        "scheme": u"ДКПП",
+                        "id": u"01.11.95",
+                        "description": u"Насіння соняшнику"
+                    }
+                ],
+                "unit": {
+                    "code": u"KGM",
+                    "name": u"кг"
+                },
+                "classification": {
+                    "scheme": u"CPV",
+                    "description": u"Sunflower seeds",
+                    "id": u"03111300-5"
+                },
+                "quantity": 2000,
+                "description": u"Насіння соняшнику"
+            },
+            {
+                "deliveryDate": {
+                    "endDate": (now + timedelta(days=17)).isoformat()
+                },
+                "additionalClassifications": [
+                    {
+                        "scheme": u"ДКПП",
+                        "id": u"01.11.84",
+                        "description": u"Насіння бавовнику"
+                    }
+                ],
+                "unit": {
+                    "code": u"KGM",
+                    "name": u"кг"
+                },
+                "classification": {
+                    "scheme": u"CPV",
+                    "description": u"Cotton seeds",
+                    "id": u"03111400-6"
+                },
+                "quantity": 3000,
+                "description": u"Насіння бавовнику"
+            }
+        ],
+        "classification": {
+            "scheme": u"CPV",
+            "description": u"Seeds",
+            "id": u"03111000-2"
+        },
+        "additionalClassifications": [
+            {
+                "scheme": u"КЕКВ",
+                "id": u"1",
+                "description": u"-"
+            }
+        ],
+        "procuringEntity": {
+            "identifier": {
+                "scheme": u"UA-EDR",
+                "id": u"111983",
+                "legalName": u"ДП Державне Управління Справами"
+            },
+            "name": u"ДУС"
+        },
+        "budget": {
+            "project": {
+                "name": u"proj_name",
+                "id": u"123"
+            },
+            "amount": 10000,
+            "amountNet": 12222,
+            "currency": u"UAH",
+            "id": u"12303111000-2",
+            "description": u"budget_description"
+        }
+    }
+
+
+test_plan_data2 = {
     "tender": {
         "procurementMethod": u"open",
         "tenderPeriod": {
